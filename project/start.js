27942ccfd0a80387bb2b88ab97adf27c0e9c338b
ybell <ybell@easyworkforce.com>
2020-05-04 14:49:17 -0400

    Added quote

--- project/start.js
+++ project/start.js
@@ -13,6 +13,31 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+
+app.get( '/quote', function ( request, response )
+{
+  var options = {
+    results: [],
+    settingsAction: {
+      type: "IFRAME",
+      width: 890,
+      height: 748,
+      uri: "https://example.com/settings-iframe-contents",
+      label: "Settings"
+    },
+    primaryAction: {
+      type: "IFRAME",
+      width: 890,
+      height: 748,
+      uri: "https://example.com/create-iframe-contents",
+      label: "Create Quote"
+    }
+  }
+  return response.json(options);
+});
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
