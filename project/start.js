eb22d85205314a405709021389d39025b5658a43
ybell <ybell@easyworkforce.com>
2020-05-12 11:49:06 -0400

    ref

--- project/start.js
+++ project/start.js
@@ -484,8 +484,18 @@ app.post( '/create-quote', async (req,res) => {
       //   console.log( '=== Succesfully Asociated Quote To Deal from HubSpot using the access token ===' );
       //   console.log( 'Associate Quote=====>', quoteDealResult )
       // } );
-      if(updatedDeal == 0 ) return res.sendStatus( 400 );
-      res.sendStatus( 200 );
+    if ( updatedDeal == 0 ) return res.sendStatus( 400 );
+  
+    let fn = (e) =>
+    { 
+      window.parent.postMessage( JSON.stringify( { 'action': 'DONE' } ), '*' );
+      window.parent.location.reload();
+    }
+  let body = (<div>
+                <div class="alert alert-success" role="alert">Quote Succesfully Created</div>
+                <button type="button" class="btn btn-primary" onclick={ fn }>Close And Refresh</button>
+              </div>);  
+    res.status( 200 ).send(body);
     
 })
 
