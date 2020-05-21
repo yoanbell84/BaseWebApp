f3da540ef31586c7111439b4c46bd7a08335b484
ybell <ybell@easyworkforce.com>
2020-05-21 15:14:38 -0400

    Ref

--- project/start.js
+++ project/start.js
@@ -642,7 +642,7 @@ const createQuoteObj = (name,title, userEmail ,contactEmail,amount) =>
     title: `Quote ${name}`,
     link: null,//`${ base_url }/quotes/${ id }`,
     createdAt: date,
-    amount:amount,
+    amount:Number(amount),
     status: "active",
     properties: [
       // {
